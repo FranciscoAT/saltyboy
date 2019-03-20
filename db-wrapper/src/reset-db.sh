6c8e0498010323fb0a6a326558e6a3bebb0b7e70
Francisco Trindade <francisco.ad.trindade@gmail.com>
2019-03-20 16:40:13 -0400

    updated reset db to dump then reset tables, also populate if specified valid populate dir

--- db-wrapper/src/reset-db.sh
+++ db-wrapper/src/reset-db.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 source setup-scripts/venv/bin/activate
-python3 setup-scripts/setup-tables.py
+python3 setup-scripts/setup-tables.py --dump --reset
+
+if [ ! -z ${1} ]; then
+    python3 setup-scripts/setup-tables.py --populate ${1}
+fi
 
