68b0518608ecaa906c11c06a9caf71beb5110d5b
Francisco Trindade <francisco.ad.trindade@gmail.com>
2019-03-21 12:28:53 -0400

    run with logging script

---
+++ run-with-logs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+RUNTIME=$(date +%F_%H-%M-%S)
+LOGSFILE=logs/${RUNTIME}
+SESSION="saltybot"
+
+send_to_env () {
+    tmux send -t ${SESSION} "${1}" ENTER
+}
+
+
+run_environment () {
+    send_to_env "cd ${1}"
+    send_to_env "npm install"
+    send_to_env "npm run start-dev &> ${LOGSFILE}.log"
+}
+
+tmux new-session -s ${SESSION} -d
+run_environment db-wrapper
+tmux split-window -h
+run_environment twitch-bot
+
+if [ "${1}" == "-a" ]; then
+    tmux -2 attach-session -d
+fi
+
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 session="saltybot"
 tmux new-session -s ${session} -d
-tmux send -t ${session} "cd db-wrapper; npm run start-dev" ENTER
+tmux send -t ${session} "cd db-wrapper; npm install; npm run start-dev" ENTER
 tmux split-window -h
-tmux send -t ${session} "cd twitch-bot; npm run start-dev" ENTER
+tmux send -t ${session} "cd twitch-bot; npm install; npm run start-dev" ENTER
 tmux -2 attach-session -d
