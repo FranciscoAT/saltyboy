e140e6c0d2aec67585c69546136f9515a83a84e5
Francisco Trindade <francisco.ad.trindade@gmail.com>
2019-03-26 15:41:54 -0400

    [wrapper-rewrite] added routes updated index.js accordingly fixed all bugs present in model.js

--- db-wrapper/src/index.js
+++ db-wrapper/src/index.js
@@ -1,160 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Pool } = require('pg');
-const fs = require('fs');
+
+// Routers
+const insertRouter = require('./routes/insert');
 
 const app = express();
 const port = 8080;
-const creds = JSON.parse(fs.readFileSync(__dirname + '/creds.json'));
-
-const pool = new Pool({
-    user: creds.user,
-    host: creds.host,
-    database: creds.database,
-    password: creds.password,
-    port: 5432
-});
-
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/add', (req, res) => {
-    let player1 = req.body.player1;
-    let player2 = req.body.player2;
-    let tier = req.body.tier;
-    let format = req.body.format;
-    let winner = req.body.winner;
-
-    let fighterID1, fighterID2, winnerID, dateID;
-
-    addDate()
-        .then((row) => {
-            dateID = row.id;
-            return selectFighterByName(player1.name)
-        })
-        .then((rows) => {
-            if (rows.length == 0) {
-                return addFighter(player1.name, tier);
-            } else {
-                return rows[0];
-            }
-        })
-        .then((row) => {
-            fighterID1 = row.id;
-            return selectFighterByName(player2.name);
-        })
-        .then((rows) => {
-            if (rows.length == 0) {
-                return addFighter(player2.name, tier);
-            } else {
-                return rows[0];
-            }
-        })
-        .then((row) => {
-            fighterID2 = row.id;
-            if (player1.name == winner) {
-                winnerID = fighterID1;
-            } else {
-                winnerID = fighterID2;
-            }
-            return addMatch(fighterID1, fighterID2, player1.bet, player2.bet, winnerID, format, dateID)
-        })
-        .then((row) => {
-            console.log("Added new match id: ", row.id);
-        })
-        .catch((err) => {
-            console.log("Something went wrong", err);
-        })
-
-    res.send();
-});
-
-function addFighter(fighterName, tier) {
-    const query = {
-        text: "INSERT INTO fighters(name, tier, asof) VALUES($1, $2, CURRENT_DATE) RETURNING *",
-        values: [fighterName, tier]
-    }
-
-    return new Promise((res, rej) => {
-        pool.query(query)
-            .then((queryResponse) => {
-                res(queryResponse.rows[0])
-            })
-            .catch((err) => {
-                rej(err);
-            });
-    });
-}
-
-function selectFighterByName(fighterName) {
-    const query = {
-        text: "SELECT * FROM fighters WHERE name = $1",
-        values: [fighterName]
-    }
-
-    return new Promise((res, rej) => {
-        pool.query(query)
-            .then((queryResponse) => {
-                res(queryResponse.rows);
-            })
-            .catch((err) => {
-                rej(err);
-            });
-    });
-}
-
-function addMatch(fighterID1, fighterID2, bet1, bet2, winnerID, format, dateID) {
-    const query = {
-        text: "INSERT INTO matches(date, fighter_1, fighter_2, bet_1, bet_2, winner, format) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
-        values: [dateID, fighterID1, fighterID2, bet1, bet2, winnerID, format]
-    };
-
-    return new Promise((res, rej) => {
-        pool.query(query)
-            .then((queryResponse) => {
-                res(queryResponse.rows[0])
-            })
-            .catch((err) => {
-                rej(err);
-            });
-    });
-}
-
-function addDate() {
-    let dateItems = getCurrentDate();
-
-    const query = {
-        text: "INSERT INTO dates(time, date, day_of_week, month, year) VALUES(CURRENT_TIME, CURRENT_DATE, $1, $2, $3) RETURNING *",
-        values: [dateItems["day_of_week"], dateItems["month"], dateItems["year"]]
-    };
-
-    return new Promise((res, rej) => {
-        pool.query(query)
-            .then((queryResponse) => {
-                res(queryResponse.rows[0]);
-            })
-            .catch((err) => {
-                rej(err);
-            });
-    });
-}
-
-function getCurrentDate() {
-    const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-    const months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
-
-    let d = new Date();
-
-    return {
-        "day_of_week": days[d.getDay()],
-        "month": months[d.getMonth()],
-        "year": d.getFullYear()
-    };
-
-}
-
+app.use('/add', insertRouter);
 
 app.listen(port, (err) => {
     if (err) {
@@ -163,4 +19,3 @@ app.listen(port, (err) => {
 
     console.log("Now listening on:", port);
 });
-
--- db-wrapper/src/models/model.js
+++ db-wrapper/src/models/model.js
@@ -10,7 +10,7 @@
 const { Pool } = require('pg');
 const fs = require('fs');
 
-const creds = JSON.parse(fs.readFileSync(__dirname + '/creds.json'));
+const creds = JSON.parse(fs.readFileSync(__dirname + '/../creds.json'));
 
 const pool = new Pool({
     user: creds.user,
@@ -23,16 +23,16 @@ const pool = new Pool({
 // ------ Common Functions ------
 
 /**
- * Exectues given query and returns the newly added row if successful else rejects
+ * Exectues given query and returns the response inside of a promise
  * 
- * @param {object} query Object for query for pool of form {text: SQL Query, values: [values, to, insert]}
+ * @param {object} query Object for query for the postgres pool
  * @returns {Promise}
  */
-function insertNewRow(query) {
+function executeQuery(query) {
     return new Promise((resolve, reject) => {
         pool.query(query)
             .then((response) => {
-                resolve(response.rows[0]);
+                resolve(response);
             })
             .catch((error) => {
                 reject(error);
@@ -41,23 +41,22 @@ function insertNewRow(query) {
 }
 
 /**
- * Executes query and returns all rows if successful select else rejects
+ * Returns a custom object containing details about the current datetime
  * 
- * @param {object} query Object for query for pool of form {text: SQL Query, values: [values, to, insert]}
- * @returns {Promise}
+ * @returns {Object}
  */
-function selectQuery(query) {
-    return new Promise((resolve, reject) => {
-        pool.query(query)
-            .then((response) => {
-                resolve(response.rows);
-            })
-            .catch((error) => {
-                reject(error);
-            })
-    });
-}
+function getCurrentDate() {
+    const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    const months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
 
+    let d = new Date();
+
+    return {
+        "day_of_week": days[d.getDay()],
+        "month": months[d.getMonth()],
+        "year": d.getFullYear()
+    };
+}
 
 
 // ------ Select Functions ------
@@ -68,13 +67,25 @@ function selectQuery(query) {
  * @param {string} fighterName The fighters name
  * @returns {Promise}
  */
-function getFighterByName(fighterName) {
+function getFightersByName(fighterName) {
     const query = {
         text: "SELECT * FROM fighters WHERE name = $1",
         values: [fighterName]
     };
 
-    return selectQuery(query);
+    return new Promise((resolve, reject) => {
+        executeQuery(query)
+            .then((result) => {
+                if (result.rows !== 'undefined') {
+                    resolve(result.rows);
+                } else {
+                    resolve([]);
+                }
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
 
 
@@ -84,8 +95,95 @@ function getFighterByName(fighterName) {
  * Inserts the given fighter. Will update fighter if rank has changed
  * @param {string} fighterName The fighters name
  * @param {string} tier The fighters current tier
+ * @returns {Promise}
  */
 function insertFighter(fighterName, tier) {
-    
+    return new Promise((resolve, reject) => {
+        getFightersByName(fighterName)
+            .then((rows) => {
+                if (rows.length == 0) {
+                    const query = {
+                        text: "INSERT INTO fighters(name, tier, asof) VALUES ($1, $2, CURRENT_DATE) RETURNING *",
+                        values: [fighterName, tier]
+                    };
+                    return executeQuery(query);
+                } else {
+                    return rows;
+                }
+            })
+            .then((response) => {
+                if (Array.isArray(response)) {
+                    resolve(response[0])
+                } else {
+                    console.log("Inserted new fighter with id: ", response.rows[0].id);
+                    resolve(response.rows[0]);
+                }
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
 
+
+/**
+ * Inserts the current date time into the date table and returns promise giving the new date row
+ * 
+ * @returns {Promise}
+ */
+function insertCurrentDate() {
+    const dateItems = getCurrentDate();
+
+    const query = {
+        text: "INSERT INTO dates(time, date, day_of_week, month, year) VALUES(CURRENT_TIME, CURRENT_DATE, $1, $2, $3) RETURNING *",
+        values: [dateItems["day_of_week"], dateItems["month"], dateItems["year"]]
+    };
+
+    return new Promise((resolve, reject) => {
+        executeQuery(query)
+            .then((response) => {
+                console.log("Inserted new date with id: ", response.rows[0].id);
+                resolve(response.rows[0]);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
+/**
+ * Inserts the a new match. Then returns a promise resolving with the new inserted row
+ * 
+ * @param {Object} fighter1 Fighter 1 contains at least id and bet
+ * @param {Object} fighter2 Fighter 2 contains at least id and bet
+ * @param {Integer} winnerID The fighter id winner
+ * @param {String} format The format of the match (tournament | matchmaking)
+ * @param {Integer} dateID The date id of the fight
+ * @returns {Promise}
+ */
+function insertMatch(fighter1, fighter2, winnerID, format, dateID) {
+    const query = {
+        text: "INSERT INTO matches(date, fighter_1, fighter_2, bet_1, bet_2, winner, format) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+        values: [dateID, fighter1.id, fighter2.id, fighter1.bet, fighter2.bet, winnerID, format]
+    };
+
+    return new Promise((resolve, reject) => {
+        executeQuery(query)
+        .then((response) => {
+            console.log("Inserted new match with id: ", response.rows[0].id);
+            resolve(response.rows[0]);
+        })
+        .catch((error) => {
+            reject(error);
+        });
+    });
+}
+
+
+// ------ Module Export ------
+
+module.exports = {
+    insertCurrentDate: insertCurrentDate,
+    insertFighter: insertFighter,
+    insertMatch: insertMatch
+};
