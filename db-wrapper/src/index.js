0550429f87cd5eeabb195744fb9c0d25d3f1d629
Francisco Trindade <francisco.ad.trindade@gmail.com>
2019-03-26 15:46:52 -0400

    [wrapper-rewrite] ground work for get odds router

--- db-wrapper/src/index.js
+++ db-wrapper/src/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 // Routers
-const insertRouter = require('./routes/insert');
+const insertRouter = require(__dirname + '/routes/insert');
+const oddsRouter = require(__dirname + '/routes/get-odds');
 
 const app = express();
 const port = 8080;
@@ -11,6 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/insert', insertRouter);
+app.use('/oods', oddsRouter);
+
 
 app.listen(port, (err) => {
     if (err) {
---
+++ db-wrapper/src/routes/get-odds.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const model = require('../models/model');
+
+module.exports = router;
