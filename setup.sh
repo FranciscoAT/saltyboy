d469259c42c51418b95e30262eaee308333d9805
Francisco Trindade <francisco.ad.trindade@gmail.com>
2021-12-17 17:01:44 -0500

    [refactor-to-python] Wipe to 0

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-session="saltybot"
-tmux new-session -s ${session} -d
-tmux send -t ${session} "cd db-wrapper; npm install; npm run start-dev" ENTER
-tmux split-window -h
-tmux send -t ${session} "cd twitch-bot; npm install; npm run start-dev" ENTER
-tmux -2 attach-session -d
