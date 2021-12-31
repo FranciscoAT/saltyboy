69957e2fde60a6c74a72be159c9c601b1267ee0f
Francisco Trindade <francisco.ad.trindade@gmail.com>
2021-12-31 01:12:35 -0500

    [extension-forreal] Extension refactor, more heavily rely on current match endpoint for data

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,16 +1,12 @@
 let loggedIn = document.getElementById("logged-in");
-let fighterRedSpan = document.getElementById("fighter-red");
-let fighterBlueSpan = document.getElementById("fighter-blue");
-let formatSpan = document.getElementById("match-format");
 let statusSpan = document.getElementById("match-status");
 let lastUpdated = document.getElementById("last-updated");
+let betConfirmed = document.getElementById("bet-confirmed");
 
-// Initialize data on load
+// Initialize data on load of popup
 chrome.storage.local.get(["status"], (result) => {
     update(result.status);
 });
-
-
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
     if (namespace != "local") {
@@ -23,12 +19,9 @@ chrome.storage.onChanged.addListener((changes, namespace) => {
 
 });
 
-
 function update(status) {
     loggedIn.innerText = status["loggedIn"];
-    fighterRedSpan.innerText = status["fighterRed"];
-    fighterBlueSpan.innerText = status["fighterBlue"];
-    formatSpan.innerText = status["format"];
+    betConfirmed.innerText = status["betConfirmed"];
     statusSpan.innerText = status["matchStatus"]
     lastUpdated.innerText = new Date().toString();
 }
--- extension/saltyboy.js
+++ extension/saltyboy.js
@@ -1,137 +1,185 @@
-const RUN_INTERVAL = 5000;
-const SALTY_BOY_URL = "https://www.salty-boy.com";
-let LAST_STATUS = null;
-let FETCHED_FIGHTER_DATA = false;
-let CURR_OUT_OF_DATE_ERR_COUNT = 0;
-
+const RUN_INTERVAL = 5000
+const SALTY_BOY_URL = 'https://www.salty-boy.com'
+let LAST_STATUS = null
+let FETCHED_FIGHTER_DATA = false
+let CURR_OUT_OF_DATE_ERR_COUNT = 0
 
 function run() {
-    let status = updateStatus();
-    if (status["loggedIn"] == false || checkStatusHealth(status) == false) {
-        return;
+    let status = updateStatus()
+    if (status['loggedIn'] == false) {
+        return
     }
-    getFighterData(status);
+    getFighterData(status)
 }
 
 function updateStatus() {
-    let loggedIn = document.getElementsByClassName("nav-text").length == 0;
-    let odds = document.getElementById("odds");
-    let fighterRed = "unknown";
-    let fighterBlue = "unknown";
-    let matchStatus = "unknown";
-    let format = "unknown";
-    let betConfirmed = false;
+    let loggedIn = document.getElementsByClassName('nav-text').length == 0
+    let odds = document.getElementById('odds')
+    let matchStatus = 'unknown'
+    let betConfirmed = false
 
     if (loggedIn) {
-        if (odds.getInnerHTML() == "" || document.getElementById("betconfirm") != null)  {
-            fighterRed = document.getElementById("player1").value;
-            fighterBlue = document.getElementById("player2").value;
-            matchStatus = "betting";
-            betConfirmed = document.getElementById("betconfirm") != null;
+        if (
+            odds.getInnerHTML() == '' ||
+            document.getElementById('betconfirm') != null
+        ) {
+            matchStatus = 'betting'
+            betConfirmed = document.getElementById('betconfirm') != null
         } else {
-            fighterRed = odds.getElementsByClassName("redtext")[0].innerText.trim();
-            fighterBlue = odds.getElementsByClassName("bluetext")[0].innerText.trim();
-            matchStatus = "ongoing";
-        }
-
-        let footerAlertText = document.getElementById("footer-alert").innerText;
-        if (footerAlertText.includes("more matches until the next tournament!")) {
-            format = "matchmaking";
-        } else if (footerAlertText == "Tournament mode start!" || footerAlertText.includes("characters are left in the bracket!")) {
-            format = "tournament";
+            matchStatus = 'ongoing'
         }
     }
 
     let status = {
-        "fighterRed": fighterRed,
-        "fighterBlue": fighterBlue,
-        "matchStatus": matchStatus,
-        "format": format,
-        "betConfirmed": betConfirmed,
-        "loggedIn": loggedIn
-    };
-
-    chrome.storage.local.set({"status": status}, () => {});
-
-    if (LAST_STATUS != status) {
-        LAST_STATUS = status;
-        FETCHED_FIGHTER_DATA = false;
+        matchStatus: matchStatus,
+        betConfirmed: betConfirmed,
+        loggedIn: loggedIn,
     }
 
-    return status;
+    chrome.storage.local.set({ status: status }, () => {})
+
+    if (LAST_STATUS != status) {
+        LAST_STATUS = status
+        FETCHED_FIGHTER_DATA = false
+    }
+
+    return status
 }
 
 function getFighterData(status) {
-    if (status["matchStatus"] != "betting" || status["betConfirmed"] == true) {
-        return;
+    if (status['matchStatus'] != 'betting' || status['betConfirmed'] == true) {
+        return
     }
 
-    FETCHED_FIGHTER_DATA = true;
+    FETCHED_FIGHTER_DATA = true
 
-    fetch(`${SALTY_BOY_URL}/current_match`, { method: "get"})
-        .then(res => res.json())
-        .then(data => {
-            placeBets(data);
+    fetch(`${SALTY_BOY_URL}/current-match`, { method: 'get' })
+        .then((res) => res.json())
+        .then((data) => {
+            placeBets(data)
         })
-        .catch(err => {
-            console.error("Something went wrong getting current match from server.", err);
-            FETCHED_FIGHTER_DATA = false;
-        });
+        .catch((err) => {
+            console.error(
+                'Something went wrong getting current match from server.',
+                err
+            )
+            FETCHED_FIGHTER_DATA = false
+        })
 }
 
 function placeBets(matchData) {
-    let wagerInput = document.getElementById("wager");
-    let fighterRedBtn = document.getElementById("player1");
-    let fighterBlueBtn = document.getElementById("player2");
+    let wagerInput = document.getElementById('wager')
+    let fighterRedBtn = document.getElementById('player1')
+    let fighterBlueBtn = document.getElementById('player2')
 
     // Last sanity check
-    if (fighterRedBtn.value != matchData["fighter_red"] || fighterBlueBtn.value != matchData["fighter_blue"]) {
-        CURR_OUT_OF_DATE_ERR_COUNT += 1;
+    if (
+        fighterRedBtn.value != matchData['fighter_red'] ||
+        fighterBlueBtn.value != matchData['fighter_blue']
+    ) {
+        CURR_OUT_OF_DATE_ERR_COUNT += 1
         if (CURR_OUT_OF_DATE_ERR_COUNT > 5) {
-            console.warn("Current Match from server is out of date", fighterRedBtn.value, fighterBlueBtn.value, matchData);
+            console.warn(
+                'Current Match from server is out of date',
+                fighterRedBtn.value,
+                fighterBlueBtn.value,
+                matchData
+            )
         }
-        return;
+        return
     }
+    CURR_OUT_OF_DATE_ERR_COUNT = 0
 
-    let balance = parseInt(document.getElementById("balance").innerText().replace(",", ""));
-    CURR_OUT_OF_DATE_ERR_COUNT = 0;
-
-    let wagerAmount = null;
-    let betColour = null;
-
-    if (matchData["match_format"] == "exhibition") {
-        // We don't want to bet on exhibition matches
-        wagerAmount = 1;
-        betColour = "red";
-    } else if (matchData["fighter_red_info"] == null || matchData["fighter_blue_info"] == null) {
-        // We cannot accurately bet on matches we have no stats for
-        wagerAmount = 1;
-        betColour = "red";
+    let balance = parseInt(
+        document.getElementById('balance').innerText.replace(',', '')
+    )
+    let betData = calculateBet(balance, matchData)
+    wagerInput.value = betData['amount'].toString()
+    if (betData['colour'] == 'red') {
+        fighterRedBtn.click()
     } else {
-        // TODO: more logic needs to go here
-    }
-
-    if (matchData["match_format"] == "tournament") {
-        // Always bet max on tournament
-        wagerAmount = balance;
-    }
-
-    // TODO: better implementation of logic
-    wagerInput.value = wagerAmount.toString();
-    if (betColour == "red") {
-        fighterRedBtn.click();
-    } else {
-        fighterBlueBtn.click();
+        fighterBlueBtn.click()
     }
 }
 
-function checkStatusHealth(status) {
-    for (const key in status) {
-        if (status[key] == "unknown") {
-            return false;
+function calculateBet(balance, matchData) {
+    /*
+     * Calculates the bet. Currently this method is extremely naive and needs improvement.
+     *
+     * It will always bet $1 red if all else fails on every single match.
+     * If it is in tournament mode it will always bet 100% of balance.
+     * If the two fighters have fought before it will base calculations on the subset of those
+     * matches played together.
+     * Otherwise it will base it on their respective win rates.
+     */
+
+    const MAX_BET_PERCENTAGE = 0.1 // TODO: allow this to be configurable
+
+    // Default to bet red with $1
+    let betData = {
+        amount: 1,
+        colour: 'red',
+    }
+
+    if (
+        matchData['fighter_red_info'] != null &&
+        matchData['fighter_blue_info'] != null
+    ) {
+        let fighterRedId = matchData['fighter_red_info']['id']
+        let fighterBlueId = matchData['fighter_blue_info']['id']
+
+        // First check to see if the two fighters have fought before
+        let redWinsVsBlue = 0
+        let redMatchesVsBlue = 0
+        for (const match of matchData['fighter_red_info']['matches']) {
+            if (
+                (match['fighter_red'] == fighterRedId &&
+                    match['fighter_blue'] == fighterBlueId) ||
+                (match['fighter_red'] == fighterBlueId &&
+                    match['fighter_blue'] == fighterRedId)
+            ) {
+                if (match['winner'] == fighterRedId) {
+                    redWinsVsBlue += 1
+                }
+                redMatchesVsBlue += 1
+            }
+        }
+
+        if (redMatchesVsBlue > 0) {
+            let redWinRateVsBlue = redWinsVsBlue / redMatchesVsBlue
+            if (redWinRateVsBlue < 0.5) {
+                betData['colour'] = 'blue'
+                betData['amount'] =
+                    (1 - redWinRateVsBlue) * balance * MAX_BET_PERCENTAGE
+            } else {
+                betData['amount'] =
+                    redWinRateVsBlue * balance * MAX_BET_PERCENTAGE
+            }
+        } else {
+            let redWinRate = matchData['fighter_red_info']['win_rate']
+            let blueWinRate = matchData['fighter_blue_info']['win_rate']
+
+            if (blueWinRate > redWinRate) {
+                betData['colour'] = 'blue'
+                betData['amount'] =
+                    (blueWinRate / (redWinRate + blueWinRate)) *
+                    balance *
+                    MAX_BET_PERCENTAGE
+            } else {
+                betData['amount'] =
+                    (redWinRate / (redWinRate + blueWinRate)) *
+                    balance *
+                    MAX_BET_PERCENTAGE
+            }
         }
     }
-    return true;
+
+    if (matchData['match_format'] == 'tournament') {
+        // Always bet max on tournament matches
+        wagerAmount = balance
+    }
+
+    return betData
 }
 
-setInterval(run, 5000);
+setInterval(run, 5000)
