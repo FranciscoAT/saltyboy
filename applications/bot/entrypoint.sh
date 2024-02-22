fbe04556c42b1e20eb127234bf3e79f36dd91f11
how2vim <francisco.ad.trindade@gmail.com>
2024-02-22 16:56:51 -0500

    fix env vars

--- applications/bot/entrypoint.sh
+++ applications/bot/entrypoint.sh
@@ -2,16 +2,5 @@
 
 set -e
 
-LOG_PATH=
-DEBUG=
-
-while getopts "l:d" name; do
-    case $name in 
-        l) LOG_PATH="$OPTARG";;
-        d) DEBUG=1;;
-        *) exit 1;;
-    esac
-done
-
 poetry run alembic upgrade head
 poetry run python main.py ${LOG_PATH:+"--logs"} "$LOG_PATH"
